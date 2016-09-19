d864f1278125ad22376c8615aa161ec028c9f034
Sofia <antonyuksofia27@gmail.com>
2016-09-19 20:46:49 +0300

    Deleted redundant

--- src/pais/Main.java
+++ src/pais/Main.java
@@ -12,10 +12,6 @@ public class Main {
             System.exit(0);
         }
         
-        /*Statement stmt = DatabaseWork.getConnection().createStatement();
-        ResultSet rs = stmt.executeQuery("select * from Plants where id = 1");
-        if (rs.next())
-        	System.out.println(rs.getString("genus"));*/
         System.out.println(Watering.getWaterededPlantsClassesById(2));
         System.out.println(Watering.getWaterededPlantsClassesByDate("'2016-08-30'"));
         System.out.println(Watering.checkIfPlantClassWasWateredOnDate("'2016-09-13'", "cacti"));
