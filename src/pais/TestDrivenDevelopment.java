b91d046542f3d87abcfe5fa150426807f6d23f00
Sofia <antonyuksofia27@gmail.com>
2016-09-19 18:32:01 +0300

    The variant of division method receiving parametres and performing checking

--- src/pais/TestDrivenDevelopment.java
+++ src/pais/TestDrivenDevelopment.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 public class TestDrivenDevelopment {
 	
 	public static double division(double a, double b) {
-		//returns the result of operation a/b
-		return a/b;
+		/*returns the result of operation a/b,
+		 * checking whether b != 0
+		 */
+		if (b != 0)
+			return a/b;
+		else
+			return 10^100;
 	}
 	
 	@Test
